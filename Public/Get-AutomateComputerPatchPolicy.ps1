45a0c5760ba31cf1094b6bd5f53b5e677e09d408
Tony Pagliaro (RFA) <tpagliaro@rfa.com>
2019-07-12 23:51:34 -0400

    new function

---
+++ Public/Get-AutomateComputerPatchPolicy.ps1
@@ -0,0 +1,53 @@
+function Get-AutomateComputerPatchPolicy {
+    [CmdletBinding()]
+    param (
+        # ComputerID of the devices(s) in question
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('id')]
+        [int[]]
+        $ComputerID
+    )
+    
+    begin {
+        #Build the URL to hit
+        $rawUri = ($global:CWAServer + '/cwa/api/v1/computers/')
+
+        $arrComputerID = [System.Collections.ArrayList]@()
+    }
+    
+    process {
+        Foreach ($id in $ComputerID) {
+            [void]($arrComputerID.Add($id))
+        }
+    }# process {
+    
+    end {
+        $arrComputerID | Invoke-Parallel {
+            $Uri = ($rawUri + $_ + '/EffectivePatchingPolicy')
+
+            $i = 0
+            Do {
+                # build the parameters
+                $i++
+                $Splat = @{
+                    Uri = $Uri
+                    Headers = $global:CWAToken
+                    ContentType = "application/json"
+                    Body = @{'page' = $i}
+                    Method = 'Get'
+                }
+
+                # Make the request
+                try {
+                    Invoke-RestMethod @Splat
+                }
+                catch {
+                    Write-Error "Failed to perform Invoke-RestMethod to Automate API with error $_.Exception.Message"
+                }
+            }# Do {
+            While ($return.count -gt 0)
+        }# $arrComputerID | Invoke-Parallel {
+    }
+}
+
