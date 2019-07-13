631b8dc77e9cd4122fd6f0c33af6b52f6e13823a
Tony Pagliaro (RFA) <tpagliaro@rfa.com>
2019-07-13 00:32:41 -0400

    new function

---
+++ Public/Get-AutomateComputerPatchingStats.ps1
@@ -0,0 +1,55 @@
+function Get-AutomateComputerPatchingStats {
+    [CmdletBinding()]
+    param (
+        # ComputerID of the devices(s) in question
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('id')]
+        [int[]]
+        $ComputerID
+    )
+    
+    begin {
+        if (! $global:CWAServer) {Write-Error '$global:CWAServer not present!'}
+
+        #Build the URL to hit
+        $rawUri = ($global:CWAServer + '/cwa/api/v1/computers/')
+
+        $arrComputerID = [System.Collections.ArrayList]@()
+    }
+    
+    process {
+        Foreach ($id in $ComputerID) {
+            [void]($arrComputerID.Add($id))
+        }
+    }# process {
+    
+    end {
+        $arrComputerID | Invoke-Parallel {
+            $Uri = ($rawUri + $_ + "/PatchingStats")
+
+            $i = 0
+            Do {
+                # build the parameters
+                $i++
+                $Splat = @{
+                    Uri = $Uri
+                    Headers = $global:CWAToken
+                    ContentType = "application/json"
+                    Body = @{'page' = $i}
+                    Method = 'Get'
+                }
+
+                # Make the request
+                try {
+                    Invoke-RestMethod @Splat
+                }
+                catch {
+                    Write-Error "Failed to perform Invoke-RestMethod to Automate API with error $_.Exception.Message"
+                }
+            }# Do {
+            While ($return.count -gt 0)
+        }# $arrComputerID | Invoke-Parallel {
+    }
+}
+
